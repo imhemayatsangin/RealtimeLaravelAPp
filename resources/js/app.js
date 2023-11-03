bc9044c1cede7e8667dc13e876258b3ebb6d0c6f
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-11-03 20:45:51 +0200

    updated app.js for UI notification changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
+import Echo from 'laravel-echo';
 import './bootstrap';
+
+window.Echo.channel('notifications').listen('UserSessionChanged', (e) => {
+
+const notificationMessage = document.getElementById('notification');
+
+notificationMessage.innerText= e.message;
+
+        notificationMessage.classList.remove('invisible');
+        notificationMessage.classList.remove('alert-success');
+        notificationMessage.classList.remove('alert-danger');
+        notificationMessage.classList.add('alert-' + e.type);
+
+
+});
+
+
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,9 +25,21 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 81,
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
+
+// import Echo from 'laravel-echo'; 
+// import Pusher from 'pusher-js';
+
+// window.Pusher = Pusher;
+
+// window.Echo = new Echo({ broadcaster: 'pusher',
+//  key: import.meta.env.VITE_PUSHER_APP_KEY, 
+// cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+//  forceTLS: true 
+// });

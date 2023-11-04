00ff210bd2dbd6c7e7f8b75fd3672956a9c63d21
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-11-04 21:39:52 +0200

    creating api and user controller

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        User::create($data);
+        return response()->json(['message' => 'User created successfully'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+        return $user;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        $user->fill($data);
+        $user->save();
+        return response()->json(['message' => 'User updated successfully']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+        $user->delte();
+        return response()->json(['message' => 'User deleted successfully']);
+    }
+}
